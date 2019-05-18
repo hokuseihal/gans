8345ac068025785411f5ec3aa53a77f7646d6647
popo <hokuseihal@gmail.com>
2019-05-18 20:59:12 +0900

    change for mul

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-N=10
-batchsize=4
-epochs=10
+N=100
+batchsize=64
+epochs=1000
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -11,7 +11,7 @@ from itertools import product
 from matplotlib import pyplot as plt
 def printtrain(epoch,epochs,datasize,d,loss):
     width=50
-    print('\repoch:[%d/%d] loss:%4.4f'%(epoch,epochs-1,loss),'='*int(width*d/datasize),'>',sep='',end='')
+    print('\repoch:[%d/%d] loss:%4.4f'%(epoch,epochs-1,loss),end='')
 def mse(x,y):
     x=x.detach().numpy()
     y=y.detach().numpy()
@@ -19,14 +19,14 @@ def mse(x,y):
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        #self.fc1=nn.Linear(2,2)
-        #self.fc2=nn.Linear(10,10)
-        self.fc3=nn.Linear(2,1)
+        self.fc1=nn.Linear(2,8)
+        self.fc2=nn.Linear(8,4)
+        self.fc3=nn.Linear(4,1)
 
 
     def forward(self, x):
-        #x=(self.fc1(x))
-        #x=(self.fc2(x))
+        x=(self.fc1(x))
+        x=(self.fc2(x))
         x=(self.fc3(x))
         return x
     def num_flat_features(self, x):
@@ -40,11 +40,12 @@ net=Net()
 
 
 criterion=nn.MSELoss()
-optimizer=optim.SGD(net.parameters(),lr=0.01,momentum=0.9)
+optimizer=optim.SGD(net.parameters(),lr=0.0000001)
+
 #make data
-data=np.array([[i,j,i+j] for i,j in product(range(N),range(N))])
+data=np.array([[i,j,i*j] for i,j in product(range(N),range(N))])
 x_train, x_test, y_train, y_test = train_test_split(data[:,0:2],data[:,2], test_size=0.33)
-x_train=torch.FloatTensor(x_train)
+x_train=torch.Tensor(x_train)
 y_train=torch.FloatTensor(y_train)
 x_test=torch.FloatTensor(x_test)
 y_test=torch.FloatTensor(y_test)
@@ -54,15 +55,13 @@ for epoch in range(epochs):
     for i in range(0,len(x_train),batchsize):
         optimizer.zero_grad()
         output=net(x_train[i:i+batchsize,:])
-        tmp=x_train[i:i+batchsize,:]
-        t=y_train[i:i+batchsize]
-        loss=criterion(output,y_train[i:i+batchsize])
+        loss=criterion(output[:,0],y_train[i:i+batchsize])
         lossl.append(loss.item())
         loss.backward()
         optimizer.step()
-        printtrain(epoch,epochs,len(x_train),i,loss.item())
+        printtrain(epoch,epochs,len(x_train),i,loss)
     print('')
 
 plt.plot(range(len(lossl)),lossl)
 plt.show()
-
+print(net.fc3.weight)
