6c2a08b56ba810c8fe941ecd7ad9f672d4715a22
hokusei-pop <pop@gmail.com>
2019-05-18 11:04:27 +0900

    first commint

---
+++ main.py
@@ -0,0 +1,68 @@
+N=10
+batchsize=4
+epochs=10
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from sklearn.model_selection import train_test_split
+import numpy as np
+from itertools import product
+from matplotlib import pyplot as plt
+def printtrain(epoch,epochs,datasize,d,loss):
+    width=50
+    print('\repoch:[%d/%d] loss:%4.4f'%(epoch,epochs-1,loss),'='*int(width*d/datasize),'>',sep='',end='')
+def mse(x,y):
+    x=x.detach().numpy()
+    y=y.detach().numpy()
+    return np.sum((x-y)**2)/x.size
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        #self.fc1=nn.Linear(2,2)
+        #self.fc2=nn.Linear(10,10)
+        self.fc3=nn.Linear(2,1)
+
+
+    def forward(self, x):
+        #x=(self.fc1(x))
+        #x=(self.fc2(x))
+        x=(self.fc3(x))
+        return x
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+net=Net()
+
+
+criterion=nn.MSELoss()
+optimizer=optim.SGD(net.parameters(),lr=0.01,momentum=0.9)
+#make data
+data=np.array([[i,j,i+j] for i,j in product(range(N),range(N))])
+x_train, x_test, y_train, y_test = train_test_split(data[:,0:2],data[:,2], test_size=0.33)
+x_train=torch.FloatTensor(x_train)
+y_train=torch.FloatTensor(y_train)
+x_test=torch.FloatTensor(x_test)
+y_test=torch.FloatTensor(y_test)
+
+lossl=[]
+for epoch in range(epochs):
+    for i in range(0,len(x_train),batchsize):
+        optimizer.zero_grad()
+        output=net(x_train[i:i+batchsize,:])
+        tmp=x_train[i:i+batchsize,:]
+        t=y_train[i:i+batchsize]
+        loss=criterion(output,y_train[i:i+batchsize])
+        lossl.append(loss.item())
+        loss.backward()
+        optimizer.step()
+        printtrain(epoch,epochs,len(x_train),i,loss.item())
+    print('')
+
+plt.plot(range(len(lossl)),lossl)
+plt.show()
+
