0392b2cf7499b1a899e8a453a2aee0e6862cdb96
popo <hokuseihal@gmail.com>
2019-05-20 08:25:10 +0900

    bug fix TODO: tuning parameter use other dataset use batchnorm?

--- gan.py
+++ gan.py
@@ -76,7 +76,7 @@ def main():
     parse.add_argument('--nocuda', action='store_false', default=True)
     parse.add_argument('--k', type=int, default=3)
     args = parse.parse_args()
-    device = 'cuda' if torch.cuda.is_available() and args.cuda else 'cpu'
+    device = 'cuda' if torch.cuda.is_available() and args.nocuda else 'cpu'
     # load data
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=True, download=True,
